c023f4d9270dabdb3d983f964561494c165ba71e
Corran Webster <cwebster@enthought.com>
2017-02-03 13:15:49 +0000

    To-do list model code and tests.

---
+++ todo-list/todo_list/model.py
@@ -0,0 +1,58 @@
+"""
+To-Do List Model Classes
+========================
+
+This module provides two classes that together provide the business logic for
+a simple to-do list application.
+"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from traits.api import (
+    Bool, HasTraits, Instance, Int, List, Property, Unicode, on_trait_change)
+
+
+class ToDoItem(HasTraits):
+    """ A task in a to-do list.
+
+    A to-do item has a description and a completed flag as its state.
+    """
+
+    #: The description of the task.
+    description = Unicode
+
+    #: Whether or no the task has been completed.
+    completed = Bool
+
+
+class ToDoList(HasTraits):
+    """ A list of tasks that need to be done.
+
+    This provides a list of :py:class:`ToDoItem` instances, as well as a list
+    of the remaining items and the count of the remaining items.
+
+    Notes
+    -----
+    This demonstrates both using :py:func:`on_trait_change` and Traits
+    :py:class:`Property` handlers to handle re-computation based on updates
+    for pedagogical reasons.
+    """
+
+    #: The list of tasks that we want to perform.
+    items = List(Instance(ToDoItem, ()))
+
+    #: The list of items that still need to be completed.
+    remaining_items = List(Instance(ToDoItem))
+
+    #: The number of remaining items.
+    remaining = Property(Int, depends_on='remaining_items')
+
+    # Trait handlers ---------------------------------------------------------
+
+    @on_trait_change('items.completed')
+    def update(self):
+        self.remaining_items = [item for item in self.items
+                                if not item.completed]
+
+    def _get_remaining(self):
+       return len(self.remaining_items)
---
+++ todo-list/todo_list/tests/test_model.py
@@ -0,0 +1,77 @@
+"""
+Tests for To-do List Model Code
+"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from unittest import TestCase
+
+from traits.testing.unittest_tools import UnittestTools
+
+from todo_list.model import ToDoItem, ToDoList
+
+
+class TestToDoItem(TestCase, UnittestTools):
+
+    def test_to_do_item(self):
+        # simple smoke test, since we have basically no logic
+        todo = ToDoItem(description='Something to do')
+
+        with self.assertTraitChanges(todo, 'completed', count=1):
+            todo.completed = True
+
+
+class TestToDoList(TestCase, UnittestTools):
+
+    def test_to_do_list(self):
+        # test that handlers work statically
+        item_1 = ToDoItem(description='Something to do')
+        item_2 = ToDoItem(description='Something else to do', completed=True)
+        todo_list = ToDoList(items=[item_1, item_2])
+
+        self.assertEqual(todo_list.remaining_items, [item_1])
+        self.assertEqual(todo_list.remaining, 1)
+
+    def test_to_do_list_completed_changed(self):
+        # test that handlers work when an item's status changes
+        item_1 = ToDoItem(description='Something to do')
+        item_2 = ToDoItem(description='Something else to do')
+        todo_list = ToDoList(items=[item_1, item_2])
+
+        with self.assertTraitChanges(todo_list, 'remaining_items', count=1):
+            with self.assertTraitChanges(todo_list, 'remaining', count=1):
+                item_2.completed = True
+
+        self.assertEqual(todo_list.remaining_items, [item_1])
+        self.assertEqual(todo_list.remaining, 1)
+
+    def test_to_do_list_changed(self):
+        # test that handlers work when the list of items changes totally
+        item_1 = ToDoItem(description='Something to do')
+        item_2 = ToDoItem(description='Something else to do', completed=True)
+        todo_list = ToDoList(items=[item_1, item_2])
+
+        item_3 = ToDoItem(description='New thing to do')
+        item_4 = ToDoItem(description='Another new thing to do')
+
+        with self.assertTraitChanges(todo_list, 'remaining_items', count=1):
+            with self.assertTraitChanges(todo_list, 'remaining', count=1):
+                todo_list.items = [item_3, item_4]
+
+        self.assertEqual(todo_list.remaining_items, [item_3, item_4])
+        self.assertEqual(todo_list.remaining, 2)
+
+    def test_to_do_list_items_changed(self):
+        # test that handlers work when the list of items is modified in-place
+        item_1 = ToDoItem(description='Something to do')
+        item_2 = ToDoItem(description='Something else to do', completed=True)
+        todo_list = ToDoList(items=[item_1, item_2])
+
+        item_3 = ToDoItem(description='New thing to do')
+
+        with self.assertTraitChanges(todo_list, 'remaining_items', count=1):
+            with self.assertTraitChanges(todo_list, 'remaining', count=1):
+                todo_list.items.append(item_3)
+
+        self.assertEqual(todo_list.remaining_items, [item_1, item_3])
+        self.assertEqual(todo_list.remaining, 2)
